1833f8370dfb3d37d4b99858ce01103c43308424
chandu141186 <chandu141186@gmail.com>
2024-07-18 16:48:34 +0530

    variable commit

---
+++ variables/sg.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "roboshop_all" {
+  name        = var.sg-name
+  description = var.sg-description
+
+
+  ingress {
+    description = "Allow All ports"
+    from_port   = var.inbound-from-port # 0 means all ports
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-all-aws"
+  }
+}
+
+
